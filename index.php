6d744dc5addeaf373a01edb481444abaa5a9bf5b
ErickRamirez2222 <erick29ram@gmail.com>
2024-12-08 05:04:11 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Administrar Clientes</title>
+</head>
+<body>
+    <h1>Bienvenido</h1>
+    <a href="create_customer.php">Agregar Cliente</a><br>
+    <a href="customer.php">Lista de Clientes</a><br>
+    <a href="create_product.php">Agregar Producto</a><br>
+    <a href="lista.php">Lista de Productos</a><br>
+</body>
+</html>
+
+
